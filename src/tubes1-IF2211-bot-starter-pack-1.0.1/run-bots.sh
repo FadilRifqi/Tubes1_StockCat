084418cd3d250c44cf5c3302693a0d567bb011a2
FadilRifqi <fadilataullahrifqi@gmail.com>
2025-05-30 17:08:25 +0700

    remove game engine and move bot folder

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python main.py --logic StockCat --email=stockcat@email.com --name=stockcat --password=123456 --team etimo --board 4 &
-python main.py --logic StockCat --email=test1@email.com --name=stima1 --password=123456 --team etimo --board 4 &
-# python main.py --logic StockCat --email=test2@email.com --name=stima2 --password=123456 --team etimo --board 1 &
-# python main.py --logic StockCat --email=test3@email.com --name=stima3 --password=123456 --team etimo --board 1 &
-# python main.py --logic StockCat --email=test4@email.com --name=stima6 --password=123456 --team etimo --board 1 &
