c079efc05f2c29a31838a14215755aa9b3e5cc0e
FadilRifqi <fadilataullahrifqi@gmail.com>
2025-05-30 17:08:09 +0700

    remove game engine and move bot folder

---
+++ src/run-bots.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python main.py --logic StockCat --email=stockcat@email.com --name=stockcat --password=123456 --team etimo --board 9 &
+python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo --board 9 &
+python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo --board 9 &
+python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo --board 9 &
+python main.py --logic Random --email=test4@email.com --name=stima6 --password=123456 --team etimo --board 9 &
